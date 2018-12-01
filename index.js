0c8f7ad6fa767ab575838232fc51a43e4dba7114
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-01 05:44:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,44 @@
+function add() {
+ 1 + 80
+}
+function substract() {
+ 60 - 40
+}
+function multiply() {
+ 2 * 3.4 
+}
+function divide() {
+ 5.0 / 2.5
+}
+
+
+
+function add(a, b) {
+  return a + b 
+
+}
+
+
+ 
+function multiply(a, b) {
+  return a * b
+}
+function subtract(a, b) {
+  return a - b
+}
+
+function divide(a, b) {
+  return a / b 
+}
+ 
+function inc(n) {
+  return n += 1  
+}  
+
+function dec(n) {
+  return n -= 1 
+}
+
+
+
+ 
